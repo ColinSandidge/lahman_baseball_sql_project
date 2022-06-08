cf1203dc0dea1f382ec04b1255082aea30208a0c
ColinSandidge <colin.sandidge@gmail.com>
2022-06-08 15:25:08 -0500

    Q10 for review

--- Q10.sql
+++ Q10.sql
@@ -4,25 +4,18 @@ least one home run in 2016. Report the players' first and last names and the num
 home runs they hit in 2016.*/
 
 --cte for hr and years played
-WITH hrhit2016 AS (SELECT playerid, yearid, MAX(hr) AS maxhr 
-			   FROM batting
-			   WHERE yearid = 2016 
-			   		 AND hr >= 1
-			   GROUP BY playerid, yearid),
-     years AS (SELECT playerid, SUM(yearid) AS sumyears
-			   FROM batting
-			   GROUP BY playerid)
+WITH hrhit2016 AS (SELECT playerid, MAX(hr) AS maxhr 
+			       FROM batting
+			       WHERE hr >= 1
+			       GROUP BY playerid)
 
-
---
-SELECT p.namefirst, 
+SELECT DISTINCT p.namefirst, 
 	   p.namelast,
-	   b.hr
-	   
-FROM batting AS b
-INNER JOIN people AS p ON b.playerid = p.playerid
-INNER JOIN hrhit2016 ON b.playerid = hrhit2016.playerid
-INNER JOIN years ON b.playerid = years.playerid
-WHERE years.sumyears >= 22121
-      AND b.hr >= hrhit2016.maxhr
-	  AND b.yearid = 2016
+	   maxhr
+	  	   
+FROM people AS p
+INNER JOIN hrhit2016 ON p.playerid = hrhit2016.playerid
+INNER JOIN batting ON p.playerid = batting.playerid
+WHERE (2016-EXTRACT(year FROM debut::date)) >= 10
+	  AND batting.yearid = 2016
+	  AND hr=maxhr
